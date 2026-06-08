876a21858164a46f6e5de9257f89cc07d1e593a4
kishorkhatiwada9-dev <kishorkhatiwada9@gmail.com>
2026-06-08 23:42:05 +0545

    Learned some concepts about lists and solved some problems.

--- tuple.py
+++ tuple.py
@@ -1,4 +1,19 @@
 #Tuple
-tup = (1,2,3,4,5,6,6,6,6,6)
-print(tup.index(2))
-print(tup.count(6))
+# tup = (1,2,3,4,5,6,6,6,6,6)
+# print(tup.index(2))
+# print(tup.count(6))
+
+#WAP to count the numebr of students with grade "A" in the following tuple
+
+# tup =("C", "D", "A", "A", "B", "B", "A")
+# tup1 = tup.count("A")
+# print(tup1)
+
+# list = list(tup1())
+# list.sort()
+# print(list)
+
+#Store the above value in a list and sort it
+# list = ["C", "D", "A", "A", "B", "B", "A"]
+# list.sort()
+# print(list)
