b54367b1727a34683cfaf3152640c4f5ae2e5360
kishorkhatiwada9-dev <kishorkhatiwada9@gmail.com>
2026-06-08 22:01:21 +0545

    i learned list in python

--- list.py
+++ list.py
@@ -1,4 +1,19 @@
-#Tuple
-tup = (1,2,3,4,5,6,6,6,6,6)
-print(tup.index(2))
-print(tup.count(6))
+#List
+
+list = [1,5,4,3,2,6]
+# list.extend([7,8])
+# print(list)
+# list.append(9)
+# print(list)
+# print(list.sort())
+# print(list)
+# print(list.sort(reverse=True))
+# print(list)
+# print(list.reverse())
+# print(list)
+list.insert(0,10)
+print(list)
+list.remove(6)
+print(list)
+list.pop(1)
+print(list)
