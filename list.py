c671e8e7b21723695fa277565d2b489c07766e7f
kishorkhatiwada9-dev <kishorkhatiwada9@gmail.com>
2026-06-09 00:03:00 +0545

    Learned some concepts of lists and solved some question

--- list.py
+++ list.py
@@ -1,19 +1,104 @@
 #List
 
-list = [1,5,4,3,2,6]
-# list.extend([7,8])                 #list.extend insert multiple emlements inside the list
+# list = [1,5,4,3,2,6]
+# # list.extend([7,8])                 #list.extend insert multiple emlements inside the list
+# # print(list)
+# # list.append(9)                     #list.append insert the element inside the parameter at the last index
+# # print(list)
+# # print(list.sort())                  #list.sort arrange the elements in ascending order
+# # print(list)
+# # print(list.sort(reverse=True))      #(list.sort(reverse=True)) arrange the elements in descending order
+# # print(list)
+# # print(list.reverse())               #(list.reverse()) reverse the elements
+# # print(list)
+# list.insert(0,10)                    #list.insert(0,10) in 0 index keeps the element 10
 # print(list)
-# list.append(9)                     #list.append insert the element inside the parameter at the last index
+# list.remove(6)                       #list.remove(6) remove the first occurence of element 6
 # print(list)
-# print(list.sort())                  #list.sort arrange the elements in ascending order
+# list.pop(1)                          #list.pop(1) removeds the element in index 1
 # print(list)
-# print(list.sort(reverse=True))      #(list.sort(reverse=True)) arrange the elements in descending order
+
+#WAP to ask the user to enter their name of 3 favourite movies and store them in a list
+# movie1 = input("Enter the name of you first fav movie: ")
+# movie2 = input("Enter the name of you second fav movie: ")
+# movie3 = input("Enter the name of you first fav movie: ")
+
+# list = []
+# list.append(movie1)
+# list.append(movie2)
+# list.append(movie3)
+# print("The three fav movies are: ",list)
+
+#Alternative
+# list = []
+# list.append(input("Enter 1st fav movie: "))
+# list.append(input("Enter 2nd fav movie: "))
+# list.append(input("Enter 3rd fav movie: "))
+
 # print(list)
-# print(list.reverse())               #(list.reverse()) reverse the elements
+
+#WAP to check if a list cotains palindrome of elements.
+# list1 = [1,2,3,2,1]
+# list2 = [1,2,3]
+
+# copy_lst1 = list1.copy()
+# copy_lst1.reverse()
+
+# if(copy_lst1 == list1):
+#     print("It is a palindromw.")
+# else:
+#     print("It is not a palindrome.")
+
+
+
+#List
+
+# list = [1,5,4,3,2,6]
+# # list.extend([7,8])                 #list.extend insert multiple emlements inside the list
+# # print(list)
+# # list.append(9)                     #list.append insert the element inside the parameter at the last index
+# # print(list)
+# # print(list.sort())                  #list.sort arrange the elements in ascending order
+# # print(list)
+# # print(list.sort(reverse=True))      #(list.sort(reverse=True)) arrange the elements in descending order
+# # print(list)
+# # print(list.reverse())               #(list.reverse()) reverse the elements
+# # print(list)
+# list.insert(0,10)                    #list.insert(0,10) in 0 index keeps the element 10
 # print(list)
-list.insert(0,10)                    #list.insert(0,10) in 0 index keeps the element 10
-print(list)
-list.remove(6)                       #list.remove(6) remove the first occurence of element 6
-print(list)
-list.pop(1)                          #list.pop(1) removeds the element in index 1
-print(list)
+# list.remove(6)                       #list.remove(6) remove the first occurence of element 6
+# print(list)
+# list.pop(1)                          #list.pop(1) removeds the element in index 1
+# print(list)
+
+#WAP to ask the user to enter their name of 3 favourite movies and store them in a list
+# movie1 = input("Enter the name of you first fav movie: ")
+# movie2 = input("Enter the name of you second fav movie: ")
+# movie3 = input("Enter the name of you first fav movie: ")
+
+# list = []
+# list.append(movie1)
+# list.append(movie2)
+# list.append(movie3)
+# print("The three fav movies are: ",list)
+
+#Alternative
+# list = []
+# list.append(input("Enter 1st fav movie: "))
+# list.append(input("Enter 2nd fav movie: "))
+# list.append(input("Enter 3rd fav movie: "))
+
+# print(list)
+
+#WAP to check if a list cotains palindrome of elements.
+# list1 = [1,2,3,2,1]
+# list2 = [1,2,3]
+
+# copy_lst1 = list1.copy()
+# copy_lst1.reverse()
+
+# if(copy_lst1 == list1):
+#     print("It is a palindromw.")
+# else:
+#     print("It is not a palindrome.")
+
