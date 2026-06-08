3d3374649d18189280744fddb99bfd21d59e3dd7
kishorkhatiwada9-dev <kishorkhatiwada9@gmail.com>
2026-06-08 22:08:02 +0545

    added the comments for easy to understand the code

--- list.py
+++ list.py
@@ -1,19 +1,19 @@
 #List
 
 list = [1,5,4,3,2,6]
-# list.extend([7,8])
+# list.extend([7,8])                 #list.extend insert multiple emlements inside the list
 # print(list)
-# list.append(9)
+# list.append(9)                     #list.append insert the element inside the parameter at the last index
 # print(list)
-# print(list.sort())
+# print(list.sort())                  #list.sort arrange the elements in ascending order
 # print(list)
-# print(list.sort(reverse=True))
+# print(list.sort(reverse=True))      #(list.sort(reverse=True)) arrange the elements in descending order
 # print(list)
-# print(list.reverse())
+# print(list.reverse())               #(list.reverse()) reverse the elements
 # print(list)
-list.insert(0,10)
+list.insert(0,10)                    #list.insert(0,10) in 0 index keeps the element 10
 print(list)
-list.remove(6)
+list.remove(6)                       #list.remove(6) remove the first occurence of element 6
 print(list)
-list.pop(1)
+list.pop(1)                          #list.pop(1) removeds the element in index 1
 print(list)
